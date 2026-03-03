73a3175d32b16540be2c7b6e4b86bb9096bd336b
anytimemusic <bima220711@gmail.com>
2026-03-03 19:29:58 +0700

    test

---
+++ file1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// deklarasi global
+int p, l, luas;
+
+// Implementasi Prosedur dan Fungsi
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
+
+int luasPersegi() {
+    return p * l;
+}
+
+void output() {
+    cout << "Hasilnya = " << luasPersegi() << endl;
+    cout << "terima kasih" << endl;
+}
+
+int main() { // mulai
+    input();
+    output();
+    return 0;
+} // selesai
