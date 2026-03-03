db7ba82781c775af92d2e7a4eef8a384cedf0f46
anytimemusic <bima220711@gmail.com>
2026-03-03 19:31:41 +0700

    test

--- rumuslingkaran.cpp
+++ rumuslingkaran.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //deklarasi variable
 float jariJari;
 float phi = 3.14159;
-//bagian fungsi
+
 void awal(){
     cout << "Masukkan jari-jari lingkaran: ";
     cin >> jariJari;
